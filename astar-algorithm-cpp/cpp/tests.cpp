3bd5847c57953ffc8726f4b7d4f723a686f542da
Robert <robert.sander@tum.de>
2019-10-23 16:20:49 +0200

    Added AStar Example

---
+++ astar-algorithm-cpp/cpp/tests.cpp
@@ -0,0 +1,16 @@
+// A unit test suite
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "stlastar.h"
+
+// TODO this test suite is a placeholder. Should include some tests of stlastar.h functionality
+
+int main(int argc, char *argv[]) {
+
+	assert(true && "failed to be true");
+
+	printf("Tests succeeded\n");
+
+}
